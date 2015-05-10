e7c2b03d9375f5b3831b99d67e63a639a92b4580
Hein van der Merwe <heinl.vdm@gmail.com>
2015-05-10 21:22:00 +0200

    Up load data

---
+++ plot2.R
@@ -0,0 +1,9 @@
+dataFile <- "./Coursera/Exploratory Data Analysis 4/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data2 <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+datetime <- strptime(paste(data2$Date, data2$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(data2$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
