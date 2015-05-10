109f6e830d8ec4fd4d3eb3db32662affec40f80f
Hein van der Merwe <heinl.vdm@gmail.com>
2015-05-10 21:21:42 +0200

    Delete plot1.R

--- plot1.R
+++
@@ -1,8 +0,0 @@
-dataFile <- "./Coursera/Exploratory Data Analysis 4/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-data2 <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
-globalActivePower <- as.numeric(data2$Global_active_power)
-png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", breaks = 25)
-dev.off()
